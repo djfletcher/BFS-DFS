8c1df55b2fcb521d7d9984f1bc86b21e9f277c79
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-13 15:49:51 -0700

    Write Queue class

---
+++ maze_path_finder.js
@@ -0,0 +1,20 @@
+
+
+class Queue {
+  constructor() {
+    this.q = [];
+    this.length = 0;
+  }
+
+  enq(el) {
+    this.length += 1;
+    return this.q.push(el);
+  }
+
+  deq() {
+    if (this.length > 0) {
+      this.length -= 1;
+      return this.q.shift();
+    }
+  }
+}
