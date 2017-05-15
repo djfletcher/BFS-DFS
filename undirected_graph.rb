dae70884770f858b17a8aa1bab11ee819ba6e390
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-15 15:09:07 -0700

    Write graph, vertex, and edge classes

---
+++ undirected_graph.rb
@@ -0,0 +1,61 @@
+class Vertex
+  attr_accessor :val, :edges, :weight
+
+  def initialize(val)
+    @val = val
+    @edges = []
+    @weight = 1
+  end
+end
+
+
+class Edge
+  attr_accessor :vertices
+
+  def initialize(v1, v2)
+    @vertices = [v1, v2]
+  end
+end
+
+
+class UndirectedGraph
+  attr_accessor :vertices, :edges
+
+  def initialize(vertices = [], edges = [])
+    @vertices = vertices
+    @edges = edges
+  end
+
+  def insert_vertex(vertex)
+    if !@vertices.include?(vertex)
+      @vertices << vertex
+      @edges.push(*vertex.edges)
+    end
+
+    vertex
+  end
+
+  def insert_edge(edge)
+    if !@edges.include?(edge)
+      @edges << edge
+      @vertices.push(*edge.vertices)
+    end
+
+    edge
+  end
+
+  def remove_vertex(vertex)
+    if @vertices.include?(vertex)
+      @vertices.delete(vertex)
+      vertex.edges.each do |edge|
+        @edges.delete(edge)
+      end
+    end
+
+    vertex
+  end
+
+  def remove_edge(edge)
+    @edges.delete(edge)
+  end
+end
