d3baa0ed37c4acb05bfeff9eb97be66b01d71864
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-15 17:33:09 -0700

    write directed_graph vertex and edge classes

---
+++ ruby/undirected_graph.rb
@@ -0,0 +1,71 @@
+require 'byebug'
+
+class Vertex
+  attr_accessor :val, :edges, :weight
+
+  def initialize(val)
+    @val = val
+    self.edges = []
+    @weight = 1
+  end
+end
+
+
+class Edge
+  attr_accessor :vertices
+
+  def initialize(v1, v2)
+    self.vertices = [v1, v2]
+    v1.edges << self
+    v2.edges << self
+  end
+
+  def destroy!
+    self.vertices.each { |v| v.edges.delete(self) }
+    self.vertices = nil
+    self
+  end
+end
+
+
+class UndirectedGraph
+  attr_accessor :vertices, :edges
+
+  def initialize(vertices = [], edges = [])
+    @vertices = vertices
+    @edges = edges
+  end
+
+  def insert_vertex(vertex)
+    if !@vertices.include?(vertex)
+      @vertices << vertex
+      @edges.push(*vertex.edges)
+    end
+
+    vertex
+  end
+
+  def insert_edge(edge)
+    if !@edges.include?(edge)
+      @edges << edge
+      @vertices.push(*edge.vertices)
+    end
+
+    edge
+  end
+
+  def remove_vertex(vertex)
+    if @vertices.include?(vertex)
+      @vertices.delete(vertex)
+      vertex.edges.each do |edge|
+        @edges.delete(edge)
+      end
+    end
+
+    vertex
+  end
+
+  def remove_edge(edge)
+    @edges.delete(edge)
+  end
+end
--- spec/undirected_graph_spec.rb
+++ spec/undirected_graph_spec.rb
@@ -26,5 +26,26 @@ describe Edge do
       expect(edge.vertices).to include(v1)
       expect(edge.vertices).to include(v2)
     end
+
+    it 'adds itself to each vertex\'s list of edges' do
+      expect(v1.edges).to include(edge)
+      expect(v2.edges).to include(edge)
+    end
   end
+end
+
+describe UndirectedGraph do
+  let(:v1) { Vertex.new('v1') }
+  let(:v2) { Vertex.new('v2') }
+  let(:v3) { Vertex.new('v3') }
+  let(:v4) { Vertex.new('v4') }
+
+  let(:e1) { Edge.new(v1, v2) }
+  let(:e2) { Edge.new(v1, v3) }
+  let(:e3) { Edge.new(v2, v4) }
+  let(:e4) { Edge.new(v3, v4) }
+  let(:e5) { Edge.new(v1, v4) }
+
+
+
 end
